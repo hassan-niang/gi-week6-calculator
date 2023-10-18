0b997ba1887cb5fee8dcde22bea5600504b6daa9
hassan-niang <hassanniang03@gmail.com>
2023-10-18 16:31:20 -0400

    foundation of calc. No CSS and little JS.

---
+++ script.js
@@ -0,0 +1,17 @@
+const n1 = document.getElementById(`1`) 
+const n2 = document.getElementById(`2`) 
+const n3 = document.getElementById(`3`) 
+const n4 = document.getElementById(`4`) 
+const n5 = document.getElementById(`5`) 
+const n6 = document.getElementById(`6`) 
+const n7 = document.getElementById(`7`) 
+const n8 = document.getElementById(`8`) 
+const n9 = document.getElementById(`9`) 
+const n0 = document.getElementById(`0`) 
+const equal = document.getElementById(`equals`) 
+const add = document.getElementById(`add`) 
+const sub = document.getElementById(`sub`) 
+const div = document.getElementById(`divide`) 
+const multi = document.getElementById(`multi`)  
+
+
